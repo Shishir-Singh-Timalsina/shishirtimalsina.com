929300efd7a6054338fe022c042989903a2b2dbf
Shishir-Singh-Timalsina <shishir.singh.timsina@gmail.com>
2019-02-23 21:26:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Heading from './components/Heading';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Heading></Heading>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Heading extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Wrk in Progress</h1>
+        <h2>Stay tuned</h2>
+      </div>
+    )
+  }
+}
