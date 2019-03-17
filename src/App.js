af44310d51701690f7a89fdd930bcd84aa43d018
Shishir-Singh-Timalsina <shishir.singh.timsina@gmail.com>
2019-03-17 19:49:05 +0200

    Few experiments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Heading from './components/Heading';
+import PersonalInfo from './components/PersonalInfo';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Heading></Heading>
+        <PersonalInfo></PersonalInfo>
       </div>
     );
   }
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,8 +4,9 @@ export default class Heading extends Component {
   render() {
     return (
       <div>
-        <h1>Wrk in Progress</h1>
-        <h2>Stay tuned</h2>
+        <h1>Work in Progress</h1>
+        <h2>Hi this is Shishir Singh Timalsina. The work is in progress so 
+          stay tuned it will be done soon. Keep figthing and keep smiling.</h2>
       </div>
     )
   }
