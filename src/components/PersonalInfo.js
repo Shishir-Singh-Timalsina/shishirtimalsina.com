4603a2bed6bc5a693ab48056f0709c93eb63c3d2
Shishir-Singh-Timalsina <shishir.singh.timsina@gmail.com>
2019-04-07 16:33:33 +0300

    practice

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import image from '../images/rt.jpg';
+
+
 
 export default class Heading extends Component {
   render() {
     return (
       <div>
-        
+        <div>
+          <img src = {image} width="700" height="500" /> 
+        </div>
       </div>
     )
   }
