764c0c1af1569059d1a4cb9d7a213d47eb5f230a
Shishir-Singh-Timalsina <shishir.singh.timsina@gmail.com>
2019-05-31 23:44:26 +0300

    simple changes

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -9,11 +9,13 @@ export default class Hobby extends Component {
         <div className = "hobby ">
             <div>
                 <h3>Hobbys</h3>
+                <div classname="hobbyList">
                   <ul>
                     <li>Photography</li>
                     <li>Guitar</li>
                     <li>Volleyball</li>
                   </ul>
+                </div>  
             </div>
         </div>
     )
