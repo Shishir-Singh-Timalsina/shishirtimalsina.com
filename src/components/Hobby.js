a583efc219237e931ebecd76d04949d4923b9e90
Shishir-Singh-Timalsina <shishir.singh.timsina@gmail.com>
2019-06-01 23:44:42 +0300

    Remove bg image and add personal youtube channel

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import '../styles/Hobby.css';
 
 
+import {Button} from 'react-bootstrap';
+
+
+
 
 export default class Hobby extends Component {
   render() {
     return (
         <div className = "hobby ">
             <div>
-                <h3>Hobbys</h3>
+                <h3>Hobbies</h3>
                 <div classname="hobbyList">
                   <ul>
                     <li>Photography</li>
                     <li>Guitar</li>
                     <li>Volleyball</li>
-                    <li><a href = "https://www.youtube.com"> Youtube </a></li>
+                    <li><a href = "https://www.youtube.com/channel/UCMzhjSdYumin3Fx5a2oK16w?view_as=subscriber">   <Button variant="outline-danger">Youtube</Button></a></li>
                   </ul>
                 </div>  
             </div>
