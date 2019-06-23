f262e53c4f4ee071db271ad23e57eb6a37593d09
Shishir-Singh-Timalsina <shishir.singh.timsina@gmail.com>
2019-06-23 23:20:34 +0300

    Basic description added

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -8,32 +8,49 @@ export default class Heading extends Component {
   render() {
     return (
       <div className = "personalInfo">
+
         <div>
           <img className = "imageone" src = {myimage} alt="My Awesome"/> 
         </div>
+
         <div className = "basicDescription">
-  
-              My name is Shishir Singh Timalsina, an information Technology student in OAMK.
-              I have been doing my Bachelors since 2014 including experience 
-              with Websites, web application (Full stack developer), software
-              development and mobile application development.!!!
-              <br/><br/><hr/>
-              Country of orgin Nepal
-              <br/>
-               DOB 1992-04-28
-              <br/><hr/>
-              Email: shishir.singh.timalsina@gmail.com
-            
+        
+        
+        My name is Shishir Singh Timalsina. <br/>
+
+          I arrived in Finland in autum of  2014. I immediately started my studies of IT engineering  at OAMK.​
+          At present I am in final years of my study.​<br/>
+
+          I am really passionate about designing and  developing applications for 
+          phones and desktop enviroments including experience 
+          with Websites, web application (Full stack developer), software
+          development and mobile application development.  I work in different aspects of Devlopment 
+          of applications. I introduce myself as a programmer but I have also worked for 
+          desiging and testing of the applications. I am an enthusiastic developer ready to design
+          and devlop systems. Adjust quickly to new environment and very good team worker.​<br/>
+
+          ​
+          In futue I would like to be a enterprenuer working in development of applications 
+          for different fields of work. ​
+          <br/><br/><hr/>
+          DOB 1992-04-28
+          <br/><hr/>
+          Email: shishir.singh.timalsina@gmail.com
+
         </div>
+  
+
         <div className ="skills">
           <h2>What can I do for you ?</h2>
           
           <ul>
-              <li>Front end developer</li>
+              <li>Web Design </li>
               <li>UI designer</li>  
+              <li>Usability Testing</li>
           </ul> 
         </div>
         <h3>The website is still under process  development process.</h3>
+
       </div>
     )
   }
